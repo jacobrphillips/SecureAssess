f7e2a0627bdd01b5e7814e22ac09ae676a399f96
Jacob Phillips <jacobrphillipsdev@gmail.com>
2024-06-03 07:44:24 +0100

    Creates Angular client

--- API/Program.cs
+++ API/Program.cs
@@ -6,14 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
